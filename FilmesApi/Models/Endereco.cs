c53689f338188b5629bec7e6ac7ab17b3eb75878
Fábio Nunes <fabionunesbr@gmail.com>
2024-03-14 20:48:11 -0300

    feat: relacao cinema x endereco

--- FilmesApi/Models/Endereco.cs
+++ FilmesApi/Models/Endereco.cs
@@ -9,5 +9,5 @@ public class Endereco
     public int Id { get; set; }
     public string logradouro { get; set; }
     public int Numero { get; set; }
-
+    public virtual Cinema Cinema { get; set; }
 }
